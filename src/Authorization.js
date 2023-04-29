e5a1cc047a270f731fe0a7911f8e9057c2965a27
elena <zavalisca.elena@gmail.com>
2023-04-29 21:33:51 +0300

    right link

--- src/Authorization.js
+++ src/Authorization.js
@@ -1,20 +1,22 @@
 import React from "react";
 import axios from "axios";
 
+const data = {
+    grant_type:'password',
+    client_id:'354FE540-6100-436F-A212-7B29C4D09545',
+    client_secret: 'rhBQCWiIufQRooTtXcH',
+    username: 'PRGUS',
+    password: 'wsDCrf7',
+    scope: 'read/write/admin',
+    refresh_token: ''
+}
+
 export default () => {
-    axios.post(`htpp://opac.liart.local/api/v1/oauth2/token`, {
+    axios.post(`http://opac.liart.local/api/v1/oauth2/token`, {
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
+            'Content-Type': 'application/www-form-urlencoded'
         },
-        body: JSON.stringify({
-            grant_type:'password',
-            client_id:'354FE540-6100-436F-A212-7B29C4D09545',
-            client_secret: 'rhBQCWiIufQRooTtXcH',
-            username: 'PRGUS',
-            password: 'wsDCrf7',
-            scope: 'read/write/admin',
-            refresh_token: ''
-        })
+        data
     })
     .then(respons => {console.log(respons)})
     .catch(err => {console.error(err)})
